bd63e87dbdc7b42cb07512f4999baabca2a3245f
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-21 22:09:44 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -20,6 +20,17 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+    eff() {
+        let eff = player.p.points.add(1).log10().mul(0.1).add(1)
+        if(hasMilestone("a",28)) eff = eff.mul(player.a.milestones.length / 25)
+        eff = eff.pow(buyableEffect("p",12))
+        if(hasUpgrade("p",33)) eff = eff.pow(upgradeEffect("p",33))
+        if(inChallenge("p",11) && !hasMilestone("a", 18)) eff = eff.div(10)
+        if(hasMilestone("a",9)) eff = eff.pow(0.5).div(hasUpgrade("f",14) ? 1 : 2)
+        if(inChallenge("p",11) && hasMilestone("a", 18)) eff = new Decimal(1)
+        return eff
+    },
+    effectDescription() { return `Base Points Gain ${format(layers.p.eff())}` },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
