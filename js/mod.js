07317212d80adf71e3e5c4dab3ced425f18af9a3
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-22 22:56:07 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -38,12 +38,13 @@ function canGenPoints(){
 
 // Calculate points/sec!
 function getPointGen() {
-let gain = new Decimal(layers.a.eff())
+let gain = new Decimal(layers.a.eff()).times(layers.m.eff())
 if (hasUpgrade("a", 12)) gain = gain.times(upgradeEffect("a", 12))
 if (hasUpgrade("a", 14)) gain = gain.times(5)
 if (hasUpgrade("a", 21)) gain = gain.times(upgradeEffect("a", 21))
 if (hasUpgrade("a", 23)) gain = gain.pow(1.25)
 if (hasUpgrade("a", 24)) gain = gain.times(100)
+if (hasUpgrade("m", 11)) gain = gain.times(5)
 	return gain
 }
 
