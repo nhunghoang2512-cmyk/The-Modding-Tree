2b85f22c7cd83fa4a7aedea3e3bc80fa6a7016c4
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-23 15:31:02 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -154,7 +154,7 @@ addLayer("m", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.05, // Prestige currency exponent
+    exponent: 0.01, // Prestige currency exponent
     gainMult() {
     	let mult = new Decimal(1)
         return mult
