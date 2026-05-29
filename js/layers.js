e626e631d612417ca82441a662730afa45b311a0
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-29 21:46:08 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -11,9 +11,10 @@ addLayer("b", {
     resource: "bigbang", // Name of prestige currency
     baseResource: "atoms", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
 	exponent() {
-		let exp = new Decimal(0.5)
+		let exp = new Decimal(1)
+		exp = exp.div(player.points)
 		return exp
 	},
     passiveGeneration() {
