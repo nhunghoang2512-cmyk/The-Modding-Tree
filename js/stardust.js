247865f43be1851aa2059e0d8d0cb2fde4e3a6f0
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-30 13:56:29 +0700

    Update stardust.js

--- js/stardust.js
+++ js/stardust.js
@@ -1,1 +1,45 @@
-
+addLayer("s", {
+    name: "stardust", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "✨", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#a6a377",
+    requires: new Decimal(1e12), // Can be a function that takes requirement increases into account
+    resource: "stardust", // Name of prestige currency
+    baseResource: "atoms", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+	exponent() {
+		let exp = new Decimal(0.5)
+		return exp
+	},
+    passiveGeneration() {
+        return 0
+    },
+    gainMult() {
+		let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+		let exp = new Decimal(1)
+        return exp
+    },
+    milestones: {
+	},
+    upgrades: {
+        11: {
+			      title: "1",
+            description: "x5 point.",
+            cost() { return new Decimal(1) },
+            unlocked() { return true },
+		},
+	},
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "s", description: "S: Reset for stardust", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})
