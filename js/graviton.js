72b7356e45e9d58743a9d588d0e3e435a346ed72
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-31 21:37:09 +0700

    Update graviton.js

--- js/graviton.js
+++ js/graviton.js
@@ -7,7 +7,7 @@ addLayer("g", {
 		points: new Decimal(0),
     }},
     color: "#a6a377",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1e9), // Can be a function that takes requirement increases into account
     resource: "graviton", // Name of prestige currency
     baseResource: "planck length", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
