2ade6b7636c82b9363baaea763da3fda5f253cdb
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-05-31 20:05:41 +0700

    Update and rename stardust.js to graviton.js

--- js/graviton.js
+++ js/graviton.js
@@ -1,6 +1,6 @@
-addLayer("s", {
+addLayer("g", {
     name: "graviton", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "✨", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
@@ -8,7 +8,7 @@ addLayer("s", {
     }},
     color: "#a6a377",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "stardust", // Name of prestige currency
+    resource: "graviton", // Name of prestige currency
     baseResource: "planck length", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -35,7 +35,7 @@ addLayer("s", {
 	},
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "s", description: "S: Reset for stardust", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "g", description: "G: Reset for graviton", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return hasMilestone('b', 1)},
 })
