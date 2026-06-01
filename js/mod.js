939ba73f89e0124482c599b84f95261e29bcd50e
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-06-01 14:35:10 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,6 +41,7 @@ function getPointGen() {
 let gain = new Decimal(1)
 if (hasUpgrade("n", 11)) gain = gain.times(2)
 if (hasUpgrade("n", 12)) gain = gain.times(upgradeEffect('n', 12))
+if (hasUpgrade("n", 13)) gain = gain.times(5)
 return gain
 }
 
