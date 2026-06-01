9747b2574175798467eabcb1aebd695a9ca69d75
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-06-01 14:59:28 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,6 +42,7 @@ let gain = new Decimal(1)
 if (hasUpgrade("n", 11)) gain = gain.times(2)
 if (hasUpgrade("n", 12)) gain = gain.times(upgradeEffect('n', 12))
 if (hasUpgrade("n", 13)) gain = gain.times(5)
+if (hasUpgrade("n", 21)) gain = gain.times(upgradeEffect('n', 21))
 return gain
 }
 
