51a8c29408de16c2c0853292dab93638b020b03a
nhunghoang2512-cmyk <nhunghoang2512@gmail.com>
2026-06-01 18:06:21 +0700

    Create graviton.js

---
+++ js/graviton.js
@@ -0,0 +1,47 @@
+addLayer("g", {
+    name: "graviton", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "white",
+    requires: new Decimal(1e9), // Can be a function that takes requirement increases into account
+    resource: "graviton", // Name of prestige currency
+    baseResource: "planck length", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+	  exponent() {
+		  let exp = new Decimal(0.1)
+		  return exp
+	  },
+    passiveGeneration() {
+        return 0
+    },
+    gainMult() {
+		  let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+		  let exp = new Decimal(1)
+        return exp
+    },
+    milestones: {
+	  },
+    upgrades: {
+        11: {
+			      title: "1",
+            description: "x6 pl gain.",
+            cost() { return new Decimal(1) },
+            unlocked() { return true },
+		    },
+	  },
+    buyables: {
+	},
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "n", description: "N: Reset for nothing", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade('n', 24)},
+})
