7f4532d35f1c08bb37e910cf6b570073f5d01194
inkonst <nhunghoang2512@gmail.com>
2026-06-09 17:37:48 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,6 +44,7 @@ if (hasUpgrade("e", 12)) gain = gain.times(upgradeEffect("e", 12))
 gain = gain.times(buyableEffect('e', 11))
 if (hasUpgrade("e", 14)) gain = gain.times(2)
 if (hasUpgrade("ea", 11)) gain = gain.times(5)
+if (hasUpgrade("ea", 13)) gain = gain.times(10)
 	return gain
 }
 
