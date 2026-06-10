efa025f7db9887223c4a68d11f423c3cc096ca4e
inkonst <nhunghoang2512@gmail.com>
2026-06-10 18:00:13 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -46,6 +46,7 @@ if (hasUpgrade("e", 14)) gain = gain.times(2)
 if (hasUpgrade("ea", 11)) gain = gain.times(5)
 if (hasUpgrade("ea", 13)) gain = gain.times(10)
 if (hasUpgrade("ea", 14)) gain = gain.times(upgradeEffect("ea", 14))
+gain = gain.times(layers.m.eff())
 	return gain
 }
 
