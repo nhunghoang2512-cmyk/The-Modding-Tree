2b14ffbd6d480a547743f739926bfc52903a6c66
inkonst <nhunghoang2512@gmail.com>
2026-06-10 16:05:31 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The ??? Tree",
 	author: "nobody",
 	pointsName: "skills",
-	modFiles: ["effortless.js", "easy.js", "tree.js"],
+	modFiles: ["effortless.js", "easy.js", "medium.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
