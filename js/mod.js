722761b6237e552aa52819618b4db14cb83213e4
inkonst <nhunghoang2512@gmail.com>
2026-07-02 17:47:30 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The ??? Tree",
 	author: "nobody",
 	pointsName: "skills",
-	modFiles: ["effortless.js", "easy.js", "medium.js", "tree.js"],
+	modFiles: ["effortless.js", "easy.js", "medium.js", "hard.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
@@ -49,6 +49,7 @@ if (hasUpgrade("ea", 14)) gain = gain.times(upgradeEffect("ea", 14))
 gain = gain.times(layers.m.eff())
 if (hasMilestone("m", 1)) gain = gain.times(100)
 gain = gain.times(buyableEffect('ea', 11))
+gain = gain.times(layers.h.eff())
 
 //challenge
 if (inChallenge('m', 11)) gain = gain.pow(0.3)
