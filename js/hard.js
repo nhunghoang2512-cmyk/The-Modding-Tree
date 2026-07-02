dc38d11638406dc9a016385b30cbd466d2bbd8f5
inkonst <nhunghoang2512@gmail.com>
2026-07-02 17:46:31 +0700

    Create hard.js

---
+++ js/hard.js
@@ -0,0 +1,52 @@
+addLayer("h", {
+    name: "hard", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "H", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FE7C00",
+    requires: new Decimal(500), // Can be a function that takes requirement increases into account
+    resource: "hard point", // Name of prestige currency
+    baseResource: "medium point", // Name of resource prestige is based on
+    baseAmount() {return player.m.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+	exponent() {
+		let exp = new Decimal(0.01)
+		return exp
+	},
+    passiveGeneration() {
+        return 0
+    },
+    gainMult() {
+		let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+		let exp = new Decimal(1)
+        return exp
+    },
+    eff() {
+		let pow = 3
+        let eff = player.h.points.add(1).pow(pow)
+        return eff
+    },
+    effectDescription() {
+    	return `boosting skill, ef by ${format(layers.h.eff())}`
+	},
+    milestones: {
+	},
+    upgrades: {
+	},
+    buyables: {
+	},
+    challenges: {
+    },
+	branches:['ea', 'm'],
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "h", description: "H: Reset for hard", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasMilestone("m", 5) || player.h.total.gte(1) || player.h.unlocked},
+})
