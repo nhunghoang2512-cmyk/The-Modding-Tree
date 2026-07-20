29695d47bb57bb6ee31a28ba16a87fd6ca6fe5ae
inkonst <nhunghoang2512@gmail.com>
2026-07-20 22:39:37 +0700

    Update stardust.js

--- js/stardust.js
+++ js/stardust.js
@@ -1,20 +1,24 @@
-addLayer("s", {
-    name: "stardust", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "✨", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("p", {
+    name: "plus", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
 		points: new Decimal(0),
     }},
-    color: "#a6a377",
+    color: "#ffffff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "stardust", // Name of prestige currency
+    resource: "plus", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
 	exponent() {
 		let exp = new Decimal(0.1)
 		return exp
+	},
+	base() {
+		let base = 10
+		return base
 	},
     passiveGeneration() {
         return 0
