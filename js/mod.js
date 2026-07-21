5559518a2283c375b570beaad6f22dd7908e2883
inkonst <nhunghoang2512@gmail.com>
2026-07-21 18:38:15 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -6,8 +6,8 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	offlineLimit: 0.1,  // In hours
 }
 
 // Set your version in num and name
